2be361408952258c4ceeae68e3745c842df190d6
Blake Trott <blaketrott2004@gmail.com>
2026-02-27 15:44:02 -0500

    Last Names

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from ultrasonic import Ultrasonic
 from camera import Camera
 import time
 
-#written by Blake, Dante, Trenton
+#written by Blake Trott, Dante Littlejohn, Trenton Lewis
 def prelim_check(led, motor, servo, infrared, ultrasonic, camera):
     try:
         print("Testing motor...")
